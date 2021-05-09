fc55ef3a80075dd8ce7c231620cad6f72de69247
GlauberBalsani <glauber_29@hotmail.com>
2021-05-09 12:01:59 -0300

    Algorimos em C fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int fat, n;
+    printf("Informe o valor para calcular o fatorial: ");
+    scanf("%d", &n);
+    
+    for (fat= 1; n>1; n = n - 1){
+         fat = fat * n;
+    }
+    	printf("%d\n", fat);
+    	return 0;
+}
